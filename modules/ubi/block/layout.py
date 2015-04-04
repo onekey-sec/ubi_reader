371e10a92fa0bff59170cb0bdfc21575d06fbd08
Stefan <sviehboeck@gmail.com>
2015-04-04 20:01:29 +0200

    find pairs based on image seq number. fixes a bug that occurs when images have the same name.

--- modules/ubi/block/layout.py
+++ modules/ubi/block/layout.py
@@ -30,21 +30,17 @@ def group_pairs(blocks, layout_blocks_list):
     Returns:
     List -- Layout block pair indexes grouped in a list
     """
-    try:
-        layouts_grouped = [[blocks[layout_blocks_list[0]].peb_num]]
 
-        for l in layout_blocks_list[1:]:
-            for lnd in layouts_grouped:
-                if blocks[l].vtbl_recs[0].name == blocks[lnd[0]].vtbl_recs[0].name:
-                    lnd.append(blocks[l].peb_num)
-                    break
-            else:
-                    layouts_grouped.append([blocks[l].peb_num])
+    image_dict={}
+    for block_id in layout_blocks_list:
+        image_seq=blocks[block_id].ec_hdr.image_seq
+        print image_seq
+        if image_seq not in image_dict:
+            image_dict[image_seq]=[block_id]
+        else:
+            image_dict[image_seq].append(block_id)
 
-        log(group_pairs, layouts_grouped)
-        return layouts_grouped
-    except Exception, e:
-        error(group_pairs, 'Fatal', e)
+    return image_dict.values()
 
 
 def associate_blocks(blocks, layout_pairs, start_peb_num):
