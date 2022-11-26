7d9bad17a1375c3423e4b63757f3d063e76a3981
AT0myks <at0myks.dev@gmail.com>
2022-11-27 00:38:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,18 +11,15 @@ setup(
     author='Jason Pruitt',
     author_email='jrspruitt@gmail.com',
     url='https://github.com/jrspruitt/ubi_reader',
-    long_description='Collection of Python scripts for reading information about and extracting data from UBI and UBIFS images.',
+    long_description='Python tool for reading information about and extracting data from UBI and UBIFS images.',
     platforms=['Linux'],
     license='GNU GPL',
     keywords='UBI UBIFS',
-
     requires=['lzo'],
     packages = find_packages(),
-    scripts=['scripts/ubireader_display_info',
-             'scripts/ubireader_extract_files',
-             'scripts/ubireader_list_files',
-             'scripts/ubireader_extract_images',
-             'scripts/ubireader_utils_info',
-             'scripts/ubireader_display_blocks',
-            ],
+    entry_points={
+        'console_scripts': [
+            'ubireader = __main__:main',
+        ]
+    }
 )
